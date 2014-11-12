a14487056d0bf0bfe162d34fec401aa494d29ddb
ankeetmaini <ankeet.maini@gmail.com>
2014-11-12 17:03:09 +0530

    added a todos route and displayed todos via fixtures

--- app/models/todo.js
+++ app/models/todo.js
@@ -1,6 +1,16 @@
 import DS from 'ember-data';
 
-export default DS.Model.extend({
+var Todo = DS.Model.extend({
     description: DS.attr('string'),
     isCompleted: DS.attr('boolean')
 });
+
+Todo.reopenClass({
+    FIXTURES: [
+        {id: 1, description: 'finish this todo-app', isCompleted: false},
+        {id: 2, description: 'eat proper dinner', isCompleted: false},
+        {id: 3, description: 'organize your life', isCompleted: false}
+    ]
+});
+
+export default Todo;
