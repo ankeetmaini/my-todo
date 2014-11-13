45bdd222c4e622065e30a33728cf121897479976
ankeetmaini <ankeet.maini@gmail.com>
2014-11-13 09:20:55 +0530

    fixed to mark the todo uncomplete after making it complete

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -3,12 +3,16 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
     isCompleted: function(key, value) {
         var model = this.get('model');
-        if(value) {
+        if(value === true) {
             model.set('isCompleted', true);
             model.save();
             return value;
-        } else {
-            return model.get('isCompleted')
+        } 
+        if(value === false){
+            model.set('isCompleted', false);
+            model.save();
+            return value;
         }
+        return model.get('isCompleted');
     }.property('isCompleted')
 });
