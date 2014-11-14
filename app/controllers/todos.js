59077870df3212bad53fe91ec50be884b8a6d97c
ankeetmaini <ankeet.maini@gmail.com>
2014-11-14 11:59:54 +0530

    updated so that spaces are not taken as new todo

--- app/controllers/todos.js
+++ app/controllers/todos.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.ArrayController.extend({
     actions: {
         add: function() {
-            this.send('createTodo', this.get('newTodo'));
+            this.send('createTodo', this.get('newTodo').trim());
             //make the field empty
             this.set('newTodo', null);
         } 
