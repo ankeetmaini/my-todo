14e9e1feef7c446bbb51dad11655732bf95bee4b
ankeetmaini <ankeet.maini@gmail.com>
2014-11-16 20:09:13 +0530

    optimized by removing redundant templates of active and completed

--- app/routes/todos/completed.js
+++ app/routes/todos/completed.js
@@ -5,5 +5,9 @@ export default Ember.Route.extend({
 		return this.store.filter('todo', function(todo){
 			return todo.get('isCompleted');
 		});
+	},
+
+	renderTemplate: function(controller) {
+		this.render('todos/index', {controller: controller});
 	}
 });
